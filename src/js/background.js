5712787f723c58ed71856c80d2bd6e8775998547
fukayatsu <fukayatsu@gmail.com>
2013-04-01 22:15:01 +0900

    set default icon on error

--- src/js/background.js
+++ src/js/background.js
@@ -3,18 +3,25 @@ chrome.browserAction.onClicked.addListener(function(tab){
 });
 
 var setStatus = function() {
-  $.get("https://status.github.com/api/last-message.json", null, function(res) {
-    var imgPaths = {
-      "good": "../img/icon-green32.png",
-      "minor": "../img/icon-orange32.png",
-      "major": "../img/icon-red32.png"
-    };
+  $.ajax({
+    url: "https://status.github.com/api/last-message.json",
+    success: function(res) {
+      var imgPaths = {
+        "good": "../img/icon-green32.png",
+        "minor": "../img/icon-orange32.png",
+        "major": "../img/icon-red32.png"
+      };
 
-    var imgPath = imgPaths[res.status] || "../img/icon-default32.png";
-    var dateStr = moment(res.created_on).format("YYYY-MM-DD HH:mm:ss");
+      var imgPath = imgPaths[res.status];
+      var dateStr = moment(res.created_on).format("YYYY-MM-DD HH:mm:ss");
 
-    chrome.browserAction.setIcon({path: imgPath});
-    chrome.browserAction.setTitle({title: res.body + " (" + dateStr +")"});
+      chrome.browserAction.setIcon({path: imgPath});
+      chrome.browserAction.setTitle({title: res.body + " (" + dateStr +")"});
+    },
+    error: function(xhr, status, error) {
+      chrome.browserAction.setIcon({path: "../img/icon-default32.png"});
+      chrome.browserAction.setTitle({title: "can't get status."});
+    }
   });
 };
 
